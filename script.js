83aeef36789a6ac3452b092a3346936ee5de8a1c
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2023-06-28 17:52:54 -0500

    Created the grid, though it is invisible

---
+++ script.js
@@ -0,0 +1,13 @@
+function gridSetup(size){
+  const container = document.querySelector('.container');
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div');
+    gridElement.classList.add('grid-element');
+    container.appendChild(gridElement);
+  }
+}
+
+gridSetup(10);
