96f54b6e8fedc1aebd2929f74bcd1f3347c5b465
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2023-06-29 01:30:39 -0500

    Added flags and set up window onload feature

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_COLOR = '#36454F'
+const DEFAULT_MODE = 1; // 1 is sketch mode, 0 is rainbow mode
+
+let currentGridSize = DEFAULT_GRID_SIZE;
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_COLOR;
+
 function gridSetup(size){
   const container = document.querySelector('.container');
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -6,8 +14,17 @@ function gridSetup(size){
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div');
     gridElement.classList.add('grid-element');
+    gridElement.addEventListener('mousedown', gridActive);
     container.appendChild(gridElement);
   }
 }
 
-gridSetup(30);
+gridSetup(DEFAULT_GRID_SIZE);
+
+function gridActive(event){
+
+}
+
+window.onload = () => {
+  gridSetup(DEFAULT_GRID_SIZE);
+}
